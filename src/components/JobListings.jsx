2a960898e3c8ec802956ded3fb36755a3891c8ed
Divyata <divyata.phadte@gmail.com>
2024-03-21 21:31:31 +0530

    json-server setup completed, useEffect and datafetching completed, conditional fetching of jobs on Home and Jobs page completed

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import jobs from '../jobs.json'
 import JobListing from './JobListing'
 
 
 const JobListings = ({isHome = false}) => { 
 
-  const jobListings = isHome? jobs.slice(0,3) : jobs
+  const [jobs, setJobs] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  //const jobListings = isHome? jobs.slice(0,3) : jobs
+  const apiUrl = isHome ? "http://localhost:8000/jobs?_limit=3" : "http://localhost:8000/jobs"
+
+  useEffect(() => {
+    const fetchJobs = async () => { 
+      try{
+        const data = await fetch(apiUrl);
+        const result = await data.json();
+        setJobs(result)
+      }catch(error){
+        console.log("Error message",error)
+      }finally {
+        setIsLoading(false)
+      }
+       
+
+
+    }
+    fetchJobs()
+
+  },[])
+
+
   return (
     <section className="bg-blue-50 px-4 py-10">
     <div className="container-xl lg:container m-auto">
@@ -13,7 +38,7 @@ const JobListings = ({isHome = false}) => {
         {isHome? 'Recent Jobs' : 'Browse Jobs'} 
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6"> 
-                { jobListings.map( job => (
+                { jobs.map( job => (
                     <JobListing key={job.id} job={ job }/>
                 ))}  
       </div>
